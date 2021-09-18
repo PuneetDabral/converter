a38bfc29465c102e04bf132e0cb5351e5105f9f1
Puneet Dabral <puneetdabral2000@gmail.com>
2021-09-18 21:43:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+//import About from './components/About';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import React, { useState } from 'react'
+
+
 
 function App() {
+  const [mode, setMode] = useState('light') ; //wheather drak mode is enable or not 
+ const toggleMode = ()=>{
+  if(mode == 'light'){
+  setMode('dark');
+  document.body.style.backgroundColor= '#042743';
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor= 'white';
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+<Navbar title="converter" mode={mode} toggleMode={toggleMode} />
+<div className="container my-3">  {/*it is a bootsrap class it takes it in center */}
+ <Textform  heading="Enter the Text to analyze below" mode={mode}/> 
+{/* <About/> */}
+
+</div>
+
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,65 @@
+import React ,{useState}from 'react'
+
+export default function Textform(props) {
+    const handleUpClick = () =>{
+      //  console.log("upper click is fire" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleloClick = () =>{
+      //  console.log("upper click is fire" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleclearClick = () =>{
+        setText('');
+    }
+
+    const handlecopyClick =() =>{
+      var text = document.getElementById('box')
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () =>{
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "));
+
+    }
+
+
+    const handleOnChange = (event) =>{
+      //  console.log("on chnage");
+        setText(event.target.value)
+    }
+    
+    const [text, setText] = useState('');  // ('enter text')you can not use text to update taht variable 
+    // setText("new text");
+    return (
+      <>
+
+        <div className="container" style={{color: props.mode===`dark`?`white`:`#042743` }}>
+
+                     <h1>{props.heading} </h1>
+                    <div className="mb-3">
+                         
+                         <textarea className="form-control"  value= {text} style={{background: props.mode===`dark`?`grey`:`white`, color:props.mode===`dark`?`white`:`#042743`}} onChange={handleOnChange} id="box" rows="8"></textarea>
+                   </div>
+                   <button className="btn btn-primary mx-1" onClick={handleUpClick}>convert to upper case </button>
+                   <button className="btn btn-primary mx-1" onClick={handleloClick}>convert to lower case </button>                  
+                   <button className="btn btn-primary mx-1" onClick={handlecopyClick}>copy text </button>
+                   <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>remove space </button>
+                   <button className="btn btn-danger mx-1" onClick={handleclearClick}>clear text </button>
+        </div>
+        <div className="container my-3" style={{color: props.mode===`dark`?`white`:`#042743` }}>
+          <h2>Your text summary</h2>
+           <p>{text.split (" ").length}words and {text.length} caracters</p>  {/*text.split gives the arrey like word */}
+          <p>{0.8*text.split (" ").length} minutes read</p>
+          <h2>privew</h2>
+          <p>{text.length>0?text:"enter something in the text box above to  privew here "}</p>
+        </div>
+        </>
+    )
+}
